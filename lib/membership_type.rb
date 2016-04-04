b508bb19f5cc57da4dfbcee7fac1336fa9bf1ea2
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-04 15:15:24 -0500

    Create membership type model

---
+++ lib/membership_type.rb
@@ -0,0 +1,6 @@
+#Monthly Dues (must be present, represented this as an integer)
+#Membership Tier (must be present; use what's on the site, or make up your own)
+
+class MembershipType < ActiveRecord::Base
+
+end
